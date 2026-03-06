c094a023ad2df45d81c05381891384cb43cd1e8d
dhrvdeshwal-art <dhrvdeshwal@gmail.com>
2026-03-06 11:44:03 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,70 @@
+# Advanced Agriculture Management Tool
+
+def agri_hleper():
+    print("--- Dhruv's Smart" 
+          Agri-Assistant ---")"
+          print("1. Sugarcane (Ganna) - Urea/
+                NPK Guide")
+                print("2. Wheat(Gehu) - Fertilizer"
+                Guide")"
+                print("3. Poplar Trees - Growth &
+                      Pest Control")
+                      print("4. Exit")
+                      
+                      choice = input ("\nApni fasal chune "
+                      (1-4): ")"
+                      ""
+                      if choice == '1'
+                      area = float(input("Kitne Acre
+                                         zameen hai? "))"
+                                         #Sugarcane logic
+                                         urea = area * 100
+                                         npk = area * 50
+                                         print(f"\n Sugarcane Report
+                                               for {area} Acre:")
+                                               print(f"\n Sugarcane Report
+                                                     for {area} Acre:")"
+                                                     print(f" - Urea ki matra: {urea}
+                                                           kg")
+              print(f" - NPK (12:32:16) ki 
+                    matra: {npk} kg")"
+                    print(" - Tip: Top Borer se 
+                          bachao ke liye monitoring zarorri
+                          hai.") #
+                          
+                          elif choice == '2'
+                          area = float(input("Kitne Acre"
+                          zameen hai? "))"
+                          # Wheat logic 
+                     urea = area * 50
+                     dap = area * 40 
+                     print(f"\n Wheat Report for 
+                           {area} Acre:")
+                           print(f'- Urea: {urea} kg")
+                                 print(f"- DAP: {dap} kg")
+
+                                 elif choice == '3'
+                                 #Poplar Tree management logic 
+                                 trees = int(input("Kitne Poplar
+                                                   ke ped hain? "))
+                                                   print(f"\n Poplar Care Guide
+                                                         for {trees} Trees:")"
+                                                         "print ("- Growth Tip:NPk ka 
+                                                         dose naye plants ke liye zaroori hai.")"
+                                                         Print("- Pest Alert: leaf"
+                                                         Defoliator ki check -up karein.")"
+                                                         ""
+                                                         "elif choice == '4':"
+                                                         print ("Bye! Kheti ki unnaai "
+                                                         Mein lege rakein.")"
+                                                         else:
+                                                         print("INvalid choice! Sai "
+                                                         "Number select karein.")
+
+                                                         # Program ko run karne ke liye 
+                                                         if __name__=="__main__":
+                                                         agri_helper()
+                                                    
+                         
+                     
+                           
